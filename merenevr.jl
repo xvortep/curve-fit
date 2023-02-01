e1e684a49287a03112e8231f5768437d73fa1087
xvortep <schl.petrovski@yahoo.com>
2023-02-01 14:54:35 +0100

    svg support, added comparison and error estimates

--- merenevr.jl
+++ merenevr.jl
@@ -1,5 +1,7 @@
 import Plots
 
+err(x, y) = (x - y)^2
+
 ADC = [800;817;846;869;901;938;981;1018;1060;1098;1138;1322;1401;1488;1583;1661;1777;1997;2217;2389;2554;2653;2716;2783;2845;2908;2970;3014;3071;3133;3206]
 dBm_expected = [0;-1;-2;-3;-4;-5;-6;-7;-8;-9;-10;-14;-16;-18;-20;-22;-25;-30;-35;-38;-40;-42;-44;-46;-48;-50;-52;-54;-56;-58;-60]
 
@@ -11,7 +13,6 @@ pic = Plots.plot(ADC,
                  label="dBm, ADC pairs",
                  title="1st order")
 
-#A = ones(31, 2) .* [dBm_expected ones(31)]
 A = ones(31, 2) .* [ADC ones(31)]
 
 #coeffs = A\ADC
@@ -22,13 +23,40 @@ firstOrder(x) = firstOrderCoeffs[1]x + firstOrderCoeffs[2]
 xs = range(minimum(ADC), maximum(ADC), 1000)
 firstOrderYs = firstOrder.(xs)
 
+firstOrderError = sum(err.(dBm_expected, firstOrder.(ADC)))
+
 pic = Plots.plot!(xs,
                   firstOrderYs,
-                  label="fit function")
+                  label="fit function, error = " * string(round(firstOrderError, digits=4)))
 
-Plots.png(pic, "fitImage1stOrder")
+Plots.svg(pic, "fitImage1stOrder")
 
-#@show pic
+#2nd order polynomial
+g(x) = [x^2  x 1]
+
+nA = reduce(vcat, g.(ADC))
+
+secondOrderCoeffs = nA \ dBm_expected
+
+secondOrder(x) = secondOrderCoeffs[1]x^2 + secondOrderCoeffs[2]x + secondOrderCoeffs[3]
+
+nPic = Plots.plot(ADC,
+                  dBm_expected,
+                  linetype=:scatter,
+                  xlabel="ADC",
+                  ylabel="dBm",
+                  label="dbm, ADC pairs",
+                  title="2nd order")
+
+secondOrderYs = secondOrder.(xs)
+
+secondOrderError = sum(err.(dBm_expected, secondOrder.(ADC)))
+
+nPic = Plots.plot!(xs,
+                   secondOrderYs,
+                   label="fit function, error = " * string(round(secondOrderError, digits=4)))
+
+Plots.svg(nPic, "fitImage2ndOrder")
 
 #3rd order polynomial
 g(x) = [x^3 x^2  x 1]
@@ -49,13 +77,13 @@ nPic = Plots.plot(ADC,
 
 thirdOrderYs = thirdOrder.(xs)
 
+thirdOrderError = sum(err.(dBm_expected, thirdOrder.(ADC)))
+
 nPic = Plots.plot!(xs,
                    thirdOrderYs,
-                   label="fit function")
+                   label="fit function, error = " * string(round(thirdOrderError, digits=4)))
 
-Plots.png(nPic, "fitImage3rdOrder")
-
-#@show nPic
+Plots.svg(nPic, "fitImage3rdOrder")
 
 #4th order polynomial
 g(x) = [x^4 x^3 x^2  x 1]
@@ -76,14 +104,37 @@ nPic = Plots.plot(ADC,
 
 fourthOrderYs = fourthOrder.(xs)
 
+fourthOrderError = sum(err.(dBm_expected, fourthOrder.(ADC)))
+
 nPic = Plots.plot!(xs,
                    fourthOrderYs,
-                   label="fit function")
+                   label="fit function, error = " * string(round(fourthOrderError, digits=4)))
 
-Plots.png(nPic, "fitImage4thOrder")
+Plots.svg(nPic, "fitImage4thOrder")
 
-#@show nPic
+nPic = Plots.plot(ADC,
+                  dBm_expected,
+                  linetype=:scatter,
+                  xlabel="ADC",
+                  ylabel="dBm",
+                  label="dbm, ADC pairs",
+                  title="comparison")
+nPic = Plots.plot!(xs,
+                   secondOrderYs,
+                   label="1st order fit function, error = " * string(round(secondOrderError, digits=4)))
+nPic = Plots.plot!(xs,
+                   secondOrderYs,
+                   label="2nd order fit function, error = " * string(round(secondOrderError, digits=4)))
+nPic = Plots.plot!(xs,
+                   thirdOrderYs,
+                   label="3rd order fit function, error = " * string(round(thirdOrderError, digits=4)))
+nPic = Plots.plot!(xs,
+                   fourthOrderYs,
+                   label="4th order fit function, error = " * string(round(fourthOrderError, digits=4)))
+
+Plots.svg(nPic, "comparison")
 
 @show firstOrderCoeffs
+@show secondOrderCoeffs
 @show thirdOrderCoeffs
 @show fourthOrderCoeffs
