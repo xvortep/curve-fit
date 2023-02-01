417967253e02b988e3d48c8190e192222162123a
xvortep <schl.petrovski@yahoo.com>
2023-02-01 13:19:22 +0100

    added 3rd and 4th order polynomial functions

--- merenevr.jl
+++ merenevr.jl
@@ -3,21 +3,87 @@ import Plots
 ADC = [800;817;846;869;901;938;981;1018;1060;1098;1138;1322;1401;1488;1583;1661;1777;1997;2217;2389;2554;2653;2716;2783;2845;2908;2970;3014;3071;3133;3206]
 dBm_expected = [0;-1;-2;-3;-4;-5;-6;-7;-8;-9;-10;-14;-16;-18;-20;-22;-25;-30;-35;-38;-40;-42;-44;-46;-48;-50;-52;-54;-56;-58;-60]
 
-pic = Plots.plot(ADC, dBm_expected, linetype=:scatter, label="dBm, ADC pairs", title="could be better with 3rd or 4th degree polynomial")
+pic = Plots.plot(ADC,
+                 dBm_expected,
+                 linetype=:scatter,
+                 xlabel="ADC",
+                 ylabel="dBm",
+                 label="dBm, ADC pairs",
+                 title="1st order")
 
 #A = ones(31, 2) .* [dBm_expected ones(31)]
 A = ones(31, 2) .* [ADC ones(31)]
 
 #coeffs = A\ADC
-coeffs = A\dBm_expected
+firstOrderCoeffs = A \ dBm_expected
 
-f(x) = coeffs[1]x + coeffs[2]
+firstOrder(x) = firstOrderCoeffs[1]x + firstOrderCoeffs[2]
 
 xs = range(minimum(ADC), maximum(ADC), 1000)
-ys = f.(xs)
+firstOrderYs = firstOrder.(xs)
 
-pic = Plots.plot!(xs, ys, label="fit function")
+pic = Plots.plot!(xs,
+                  firstOrderYs,
+                  label="fit function")
 
-Plots.png(pic, "fitImage")
+Plots.png(pic, "fitImage1stOrder")
 
-@show pic
+#@show pic
+
+#3rd order polynomial
+g(x) = [x^3 x^2  x 1]
+
+nA = reduce(vcat, g.(ADC))
+
+thirdOrderCoeffs = nA \ dBm_expected
+
+thirdOrder(x) = thirdOrderCoeffs[1]x^3 + thirdOrderCoeffs[2]x^2 + thirdOrderCoeffs[3]x + thirdOrderCoeffs[4]
+
+nPic = Plots.plot(ADC,
+                  dBm_expected,
+                  linetype=:scatter,
+                  xlabel="ADC",
+                  ylabel="dBm",
+                  label="dbm, ADC pairs",
+                  title="3rd order")
+
+thirdOrderYs = thirdOrder.(xs)
+
+nPic = Plots.plot!(xs,
+                   thirdOrderYs,
+                   label="fit function")
+
+Plots.png(nPic, "fitImage3rdOrder")
+
+#@show nPic
+
+#4th order polynomial
+g(x) = [x^4 x^3 x^2  x 1]
+
+nA = reduce(vcat, g.(ADC))
+
+fourthOrderCoeffs = nA \ dBm_expected
+
+fourthOrder(x) = fourthOrderCoeffs[1]x^4 + fourthOrderCoeffs[2]x^3 + fourthOrderCoeffs[3]x^2 + fourthOrderCoeffs[4]x + fourthOrderCoeffs[5]
+
+nPic = Plots.plot(ADC,
+                  dBm_expected,
+                  linetype=:scatter,
+                  xlabel="ADC",
+                  ylabel="dBm",
+                  label="dbm, ADC pairs",
+                  title="4th order")
+
+fourthOrderYs = fourthOrder.(xs)
+
+nPic = Plots.plot!(xs,
+                   fourthOrderYs,
+                   label="fit function")
+
+Plots.png(nPic, "fitImage4thOrder")
+
+#@show nPic
+
+@show firstOrderCoeffs
+@show thirdOrderCoeffs
+@show fourthOrderCoeffs
